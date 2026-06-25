e0fd344e7a5cf8fed508d0d327f76a3e4cbe8427
Tripti Pandey <triptip033@gmail.com>
2026-06-25 15:13:52 +0530

    Update AuthProvider to use array for context value
    Refactor AuthProvider to provide userData and setUserData.

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -3,26 +3,26 @@ import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
 
 export const AuthContext = createContext()
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+    // localStorage.clear()
 
-  const [userData, setUserData]=useState(null)
+    const [userData, setUserData] = useState(null)
 
-  useEffect(
-    () => {
-      setLocalStorage()
-      const {employees,admin}=getLocalStorage()
-      setUserData({employees,admin})
-    }, []
-  )
+    useEffect(() => {
+        setLocalStorage()
+        const {employees} = getLocalStorage()
+        setUserData(employees)
+    }, [])
+    
+    
 
-
-  return (
-    <div>
-      <AuthContext.Provider value ={userData}>
-      {children}
-      </AuthContext.Provider>
-    </div>
-  )
+    return (
+        <div>
+            <AuthContext.Provider value={[userData,setUserData]}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    )
 }
 
 export default AuthProvider
